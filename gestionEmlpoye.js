145c537d2ccf28aa3d8f4521cb544b2cb02557eb
Ousmanly <lyousmane492@gmail.com>
2024-09-06 05:30:54 +0200

    Assignation class

--- gestionEmlpoye.js
+++ gestionEmlpoye.js
@@ -1,5 +1,4 @@
 export class Employe {
-    constructor() {}
   
     createEmploye(employe) {
       this.nom = employe.nom;
@@ -36,40 +35,63 @@ export class Employe {
     }
   }
 
-  export class Tache {
-    constructor() {}
-  
-    createTache(tache) {
-      this.nom = tache.nom;
-      this.description = tache.description;
-      this.dateDebut = tache.dateDebut;
-      this.dateFin = tache.dateFin;
-      this.statut = tache.statut;
-      this.priorite = tache.priorite;
-      console.log(`Tache: ${this.nom}, ${this.description}, ${this.dateDebut}, ${this.dateFin}, ${this.statut}, ${this.priorite} has been created`);
+export class Tache {
+
+  createTache(tache) {
+    this.nom = tache.nom;
+    this.description = tache.description;
+    this.dateDebut = tache.dateDebut;
+    this.dateFin = tache.dateFin;
+    this.statut = tache.statut;
+    this.priorite = tache.priorite;
+    console.log(`Tache: ${this.nom}, ${this.description}, ${this.dateDebut}, ${this.dateFin}, ${this.statut}, ${this.priorite} has been created`);
+}
+
+  destroyTache() {
+    this.nom = null;
+    this.description = null;
+    this.dateDebut = null;
+    this.dateFin = null;
+    this.statut = null;
+    this.priorite = null;
+    console.log(`Tache has been deleted`);
   }
-  
-    destroyTache() {
-      this.nom = null;
-      this.description = null;
-      this.dateDebut = null;
-      this.dateFin = null;
-      this.statut = null;
-      this.priorite = null;
-      console.log(`Tache has been deleted`);
-    }
-  
-    editTache(newTache) {
-      this.nom = newTache.nom;
-      this.description = newTache.description;
-      this.dateDebut = newTache.dateDebut;
-      this.dateFin = newTache.dateFin;
-      this.statut = newTache.statut;
-      this.priorite = newTache.priorite;
-      console.log(`Tache: ${this.nom}, ${this.description}, ${this.dateDebut}, ${this.dateFin}, ${this.statut},${this.priorite} has been updated`);
-    }
-    getTache() {
-      console.log(`Tache: ${this.nom}, ${this.description}, ${this.dateDebut}, ${this.dateFin}, ${this.statut},${this.priorite}`);
-      return { nom: this.nom, description: this.description , dateDebut: this.dateDebut, dateFin: this.dateFin, statut: this.statut, priorite: this.priorite };
-    }
+
+  editTache(newTache) {
+    this.nom = newTache.nom;
+    this.description = newTache.description;
+    this.dateDebut = newTache.dateDebut;
+    this.dateFin = newTache.dateFin;
+    this.statut = newTache.statut;
+    this.priorite = newTache.priorite;
+    console.log(`Tache: ${this.nom}, ${this.description}, ${this.dateDebut}, ${this.dateFin}, ${this.statut},${this.priorite} has been updated`);
   }
+  getTache() {
+    console.log(`Tache: ${this.nom}, ${this.description}, ${this.dateDebut}, ${this.dateFin}, ${this.statut},${this.priorite}`);
+    return { nom: this.nom, description: this.description , dateDebut: this.dateDebut, dateFin: this.dateFin, statut: this.statut, priorite: this.priorite };
+  }
+}
+
+export class Assignation {
+  assignerTache(assignation){
+    this.employe = assignation.employe;
+    this.tache = assignation.tache;
+    this.dateAssignation = assignation.dateAssignation;
+    console.log(`Assignation ${this.employe}, ${this.tache}, ${this.dateAssignation}`)
+  }
+
+  obtenirTachesAssignées(){
+    console.log(`Tache assigner: ${this.tache}, ${this.employe}, ${this.dateAssignation}`);
+    return {tache: this.tache,  employe: this.employe, dateAssignation: this.dateAssignation };
+  }
+
+  obtenirEmployésAssignés(){
+    console.log(`Tache assigner: ${this.employe}, ${this.tache}, ${this.dateAssignation}`);
+    return {employe: this.employe, tache: this.tache, dateAssignation: this.dateAssignation };
+  }
+
+  retirerTâche(){
+    this.tache = null;
+    console.log(`Tache has been deleted`);
+  }
+}
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express"
 import bodyParser from 'body-parser'
-import { Employe, Tache } from "./gestionEmlpoye.js";
+import { Assignation, Employe, Tache } from "./gestionEmlpoye.js";
 
 const app = express();
 const port = 3002;
@@ -16,21 +16,26 @@ app.post("/app", (req, res) => {
     emlpoye1.createEmploye({ nom: "Ly", prenom: "Ousmane", email: "ousmane492@gmail.com", poste: "fullstack developper", dateEmbauche: "2024-10-12", statut:"Senior"})
     const emlpoye2 = new Employe()
     emlpoye2.createEmploye({ nom: "Bradji", prenom: "Assa", email: "asa@gmail.com", poste: "fullstack developper", dateEmbauche: "2024-10-12", statut:"Senior"})
-    
+    let employes = [emlpoye1, emlpoye2]
 
     //tache
     const tache1 = new Tache()
     tache1.createTache({nom: "README", description: "Redaction du README", dateDebut: "2024-03-23", dateFin: "2024-05-23", statut:"Status", priorite: "Haut"})
-    
     const tache2 = new Tache()
     tache2.createTache({nom: "Front End", description: "Développement Front End", dateDebut: "2024-03-23", dateFin: "2024-05-23", statut:"Status", priorite: "Haut"})
-    
-    let status = 200;
-    let employes = [emlpoye1, emlpoye2]
     let taches = [tache1, tache2]
+    
+    //assignation
+    const a1 = new Assignation()
+    a1.assignerTache({employe: emlpoye1, tache:tache1, dateAssignation: "2024-23-01" })
+    const a2 = new Assignation()
+    a2.assignerTache({employe: emlpoye2, tache:taches, dateAssignation: "2024-03-01" })
+    let assignations = [a1, a2]
 
-    let message = {"Tache": taches, "Employe": employes}
-    if (!taches && !employes) {
+    let status = 200;
+
+    let message = {"Tache": taches, "Employe": employes, "Assignation": assignations}
+    if (!taches && !employes && !assignations) {
         message = "Not found";
         status = 400;
     }
@@ -62,17 +67,21 @@ app.put("/app", (req, res) =>{
 app.get("/app", (req, res) =>{
     const emlpoye2 = new Employe()
     emlpoye2.createEmploye({ nom: "Thimbo", prenom: "Abderrahman", email: "adad@gmail.com", poste: "frontEnd developper", dateEmbauche: "2024-10-12", statut:"Senior"})
-    
+    let employes = emlpoye2.getEmploye()
 
     const tache2 = new Tache()
     tache2.createTache({nom: "Maquette", description: "Disigner l'application", dateDebut: "2024-03-23", dateFin: "2024-05-23", statut:"Status", priorite: "Haut"})
-
-    
-    let status = 200;
-    let employes = emlpoye2.getEmploye()
     let taches = tache2.getTache()
-    let message = {"Tache": taches, "Employe": employes}
-    if (!taches && !employes) {
+
+    const a2 = new Assignation()
+    a2.assignerTache({employe: employes, tache: taches, dateAssignation: "2024-23-01" })
+    let TacheAssignations = a2.obtenirTachesAssignées();
+    let EmployeAssignation = a2.obtenirEmployésAssignés()
+
+    let status = 200;
+ 
+    let message = {"Tache": taches, "Employe": employes, "Tache assigner": TacheAssignations, "Employe assigner": EmployeAssignation}
+    if (!taches && !employes && !TacheAssignations && !EmployeAssignation) {
         message = "Not found";
         status = 400;
     }
@@ -84,16 +93,22 @@ app.delete("/app", (req, res) =>{
     const emlpoye2 = new Employe()
     emlpoye2.createEmploye({ nom: "Thimbo", prenom: "Abderrahman", email: "adad@gmail.com", poste: "frontEnd developper", dateEmbauche: "2024-10-12", statut:"Senior"})
     emlpoye2.destroyEmploye()
+    let employes = emlpoye2;
 
     const tache2 = new Tache()
     tache2.createTache({nom: "Maquette", description: "Disigner l'application", dateDebut: "2024-03-23", dateFin: "2024-05-23", statut:"Status", priorite: "Haut"})
     tache2.destroyTache()
-    
-    let status = 200;
-    let employes = emlpoye2;
     let taches = tache2;
-    let message = {"Tache": taches, "Employe": employes}
-    if (!taches && !employes) {
+
+    const a2 = new Assignation()
+    a2.assignerTache({employe: emlpoye2, tache: tache2, dateAssignation: "2024-23-01" })
+    a2.retirerTâche()
+    let assignation = a2;
+
+    let status = 200;
+    
+    let message = {"Tache": taches, "Employe": employes, "Assignation": assignation}
+    if (!taches && !employes && !assignation) {
         message = "Not found";
         status = 400;
     }
